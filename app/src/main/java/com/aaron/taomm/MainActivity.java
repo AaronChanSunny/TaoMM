aab0e9c288130e14ea470dfe800ffd8f8223aaac
aaronchansunny <aaronchansunny@gmail.com>
2015-06-27 00:13:00 +0800

    replace ListView of RecycleView

--- app/src/main/java/com/aaron/taomm/MainActivity.java
+++ app/src/main/java/com/aaron/taomm/MainActivity.java
@@ -1,17 +1,26 @@
 package com.aaron.taomm;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] mDatas;
+    private RecyclerView mRecycleView;
+    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +30,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mDatas = new String[]{"Start", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C"};
+        initDatas();
 
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDatas));
+        mRecycleView = (RecyclerView) findViewById(R.id.recycleView);
+        mRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        mRecycleView.setAdapter(new SimpleAdapter(this, mDatas));
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
     }
 
     @Override
@@ -43,4 +60,45 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    static class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+
+        Context mContext;
+        List<String> mDatas;
+
+        public SimpleAdapter(Context context, List<String> mDatas) {
+            this.mContext = context;
+            this.mDatas = mDatas;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(mContext).inflate(android.R.layout
+                    .simple_list_item_1, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.textView.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        static public class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView textView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(android.R.id.text1);
+            }
+
+        }
+
+    }
+
 }
